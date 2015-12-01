dd60539bc7a507435e153f7b7cd0c75aac3a0bbe
root <root@zzz.(none)>
2015-12-02 07:12:44 +0800

    添加日志

--- shell/smzdm.sh
+++ shell/smzdm.sh
@@ -2,51 +2,63 @@
 
 name=$1;
 password=$2;
+dir=/home/shell/smzdm;
+log=${dir}/log.log;
 
 if [ -z "$name" -o -z "$password" ] ;then
  printf "\n请输入帐户和密码\n";
  exit;
 fi
 
+random=$(awk 'BEGIN{srand();sum=rand()*10;printf("%d\n",sum)}');
+printf "\n\n\n---------------开始任务---------------\n\n\n" >> $log;
+date >> $log;
+printf "休眠"${random}"秒\n" >> $log;
+sleep $random;
+date >> $log;
+
 # 登录
-printf "\n开始登录\n"
+printf "\n开始登录\n" >> $log;
 curl \
- -c ./cookie_smzdm.txt \
- -b ./cookie_smzdm.txt \
+ -c ${dir}/cookie_smzdm.txt \
+ -b ${dir}/cookie_smzdm.txt \
  -F username=${name} \
  -F password=${password} \
  -F rememberme="on" \
  -F captcha="" \
  -F redirect_url="http://www.smzdm.com" \
- -o index.html \
+ -o ${dir}/index.html \
  -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36" \
  http://zhiyou.smzdm.com/user/login/ajax_check
 
-printf "\n登录成功\n"
+printf "\n登录成功\n" >> $log;
 
 
 # 获取用户信息
-printf "\n尝试获取用户信息\n"
+printf "\n尝试获取用户信息\n" >> $log;
  curl \
- -c ./cookie_smzdm.txt \
- -b ./cookie_smzdm.txt \
- -o userinfo.json \
+ -c ${dir}/cookie_smzdm.txt \
+ -b ${dir}/cookie_smzdm.txt \
+ -o ${dir}/userinfo.json \
  -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36" \
  http://zhiyou.smzdm.com/user/info/jsonp_get_current
 
  printf "\n"
- sed -r $'1s/^|$/\'/g' userinfo.json | xargs printf
+ sed -r $'1s/^|$/\'/g' ${dir}/userinfo.json | xargs printf >> $log;
  printf "\n"
 
 # 签到
-printf "\n尝试签到\n"
+printf "\n\n尝试签到\n" >> $log;
  curl \
- -c ./cookie_smzdm.txt \
- -b ./cookie_smzdm.txt \
- -o qiandao.json \
+ -c ${dir}/cookie_smzdm.txt \
+ -b ${dir}/cookie_smzdm.txt \
+ -o ${dir}/qiandao.json \
  -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36" \
  http://www.smzdm.com/user/qiandao/jsonp_checkin
 
  printf "\n"
- sed -r $'1s/^|$/\'/g' qiandao.json | xargs printf
+ sed -r $'1s/^|$/\'/g' ${dir}/qiandao.json | xargs printf >> $log;
  printf "\n"
+
+printf "\n\n\n---------------任务完成----------------\n\n\n" >> $log;
+
